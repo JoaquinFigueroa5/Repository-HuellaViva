dd454c4917ce9b53fca0c5b4ac793fef50c7ea0d
JoaquinFigueroa5 <kinchin.figueroa@gmail.com>
2026-04-13 16:40:13 -0600

    Speed insights added

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { Toaster } from '@/components/ui/sonner.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import App from '@/App.jsx'
 import '@/index.css'
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <TooltipProvider delayDuration={180}>
         <Analytics />
+        <SpeedInsights />
         <Toaster position="bottom-center" richColors theme="system" />
         <App />
       </TooltipProvider>
