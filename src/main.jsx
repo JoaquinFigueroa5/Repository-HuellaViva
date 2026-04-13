03abbb56e3c7b90e959567fe0387ddb27a916f03
JoaquinFigueroa5 <kinchin.figueroa@gmail.com>
2026-04-13 16:11:35 -0600

    Fix: Vercel analytics import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { Toaster } from '@/components/ui/sonner.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import App from '@/App.jsx'
 import '@/index.css'
 
@@ -11,8 +11,8 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <TooltipProvider delayDuration={180}>
-        <Toaster position="bottom-center" richColors theme="system" />
         <Analytics />
+        <Toaster position="bottom-center" richColors theme="system" />
         <App />
       </TooltipProvider>
     </BrowserRouter>
